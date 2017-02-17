aaad5ad4e7fd3bad971c0081294313dfa8013e7f
Christian Paling <christian.paling@googlemail.com>
2017-02-17 20:51:51 +0100

    Adapt printer to new notes file implementation

--- lib/annoyme/printer.rb
+++ lib/annoyme/printer.rb
@@ -1,21 +1,20 @@
-require_relative 'notes_file'
 require_relative 'logger'
 
 module Annoyme
-
   class Printer
+    def initialize(file)
+      @file = file
+    end
 
-    def self.print
-      notes = ConfigFile.parse
+    def print
+      notes = @file.parse
 
       return if notes.empty?
-      
+
       puts ""
       Logger.colored("TODO\n", :cyan)
       Logger.print_array_colorful(notes, with_index: true)
       puts ""
     end
-
   end
-
 end
